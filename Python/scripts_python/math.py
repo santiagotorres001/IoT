28f1b71e3f0b4ac2182f81317cd30c8df697c47c
Santiago <felipetorres260@gmail.com>
2024-09-02 20:53:14 -0500

    update IoT

---
+++ Python/scripts_python/math.py
@@ -0,0 +1,23 @@
+#Developer: Santiago
+#Date: 26082024
+
+'''
+Este programa te permite sumar dos numeros
+'''
+
+n1 = 15
+n2 = 15
+add = n1 + n2
+
+#print(add)
+print("La respuesta es:" ,add)
+
+print("La respuesta es:"+ str(add))
+
+print (f"La respuesta es {add}")
+
+print (f"La respuesta es {n1+n2}")
+
+
+
+
