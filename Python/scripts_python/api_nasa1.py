f98d70eb4176cc11bdbc7e7a6a85e5e33d013703
Santiago <felipetorres260@gmail.com>
2024-09-02 21:52:37 -0500

    Update Django(Server)

---
+++ Python/scripts_python/api_nasa1.py
@@ -0,0 +1,7 @@
+'''
+API: Application Programming Interface
+Nasa API: https://api.nasa.gov
+
+Ruta aleatoria de API Nasa
+https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={H6DcXN4k0GjgogigilANSzbf1QqVebU2L8vqdfxv}
+'''
